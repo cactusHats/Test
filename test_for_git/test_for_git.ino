6c4b6f9de9b941d7e09c2e014a44a36774c0848f
TTRist <64200136+TTRist@users.noreply.github.com>
2020-10-21 17:48:34 +0900

    make project v1
    つくったで

---
+++ test_for_git/test_for_git.ino
@@ -0,0 +1,22 @@
+void setup() {
+  pinMode(13, OUTPUT);
+
+  digitalWrite(13, 1);
+  delay(100);
+  digitalWrite(13, 0);
+  delay(100);
+  digitalWrite(13, 1);
+  delay(100);
+  digitalWrite(13, 0);
+  delay(100);
+  digitalWrite(13, 1);
+  delay(100);
+  digitalWrite(13, 0);
+  delay(100);
+
+}
+
+void loop() {
+
+
+}
